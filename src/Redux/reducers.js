4a0fe5639aed3870a22a3a70b56b38a2d7d62aef
hervesson <hervessonporto@hotmail.com>
2022-01-23 18:50:59 -0300

    Sagas Events

--- src/Redux/reducers.js
+++ src/Redux/reducers.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 
 import Auth from './auth/reducers';
-import Chat from './chat/reducers';
+import Events from './events/reducers';
 import Tabs from './tabs/reducers';
 
 export default combineReducers({
     Auth,
-    Chat,
+    Events,
     Tabs
 });
--- src/Redux/sagas.js
+++ src/Redux/sagas.js
@@ -1,11 +1,11 @@
 import { all } from 'redux-saga/effects';
 import authSaga from './auth/saga';
-import chatSaga from './chat/saga';
+import eventsSaga from './events/saga';
 
 
 export default function* rootSaga(getState) {
     yield all([
         authSaga(),
-        chatSaga()
+        eventsSaga()
     ]);
 }
