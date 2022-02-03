2fba9a150485c292a22c49bdf191f3c4332ae66f
hervesson <hervessonporto@hotmail.com>
2022-02-03 15:35:40 -0300

    Adicionado Tour

--- src/Cenas/Hangouts/index.js
+++ src/Cenas/Hangouts/index.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import {
   View,
@@ -8,13 +8,18 @@ import {
   TouchableOpacity,
   ImageBackground,
   SafeAreaView,
-  Platform
+  Platform,
+  Modal,
+  Alert
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { BackgroundClaro, Titulo, Descricao, Primary, Background } from '../../Styles';
 import BackButtonSolid from '../../Components/Buttons/BackButtonSolid'
+import NoMoreCards from '../../Components/NoMoreCards'
 import { BlurView, VibrancyView } from "@react-native-community/blur";
-import Carousel from 'react-native-snap-carousel';
+import CardStack, { Card } from 'react-native-card-stack-swiper';
+import CardItem from './CardItem';
+import Tour from '../../Components/Tour'
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -29,62 +34,29 @@ import { toggle_tab } from '../../Redux/actions';
 import { translate } from '../../Locales'
 const t = translate
 
+const fullHeight = Dimensions.get('window').height;
+const height = fullHeight / 3
+
 const index = props => {
-	const [data, setData] = useState([])
-	const [select, setSelect] = useState(0)
-	const [cont, setCont] = useState(0)
-	const [intereses, setInteresses] = useState([])
-	const [selectInter, setSelectInter] = useState([])
+	const [data, setData] = useState([]);
+	const [select, setSelect] = useState(0);
+	const [cont, setCont] = useState(0);
+	const [intereses, setInteresses] = useState([]);
+	const [selectInter, setSelectInter] = useState([]);
+	const [modalVisible, setModalVisible] = useState(false);
 
 	useEffect(() => {
 		helpersHangouts.GetListHangout().then(response => {
 			setData(response.data)
-		})
+		});
+		setModalVisible(true)
 	}, [])
 
 	useEffect(() => {
 		helpersInterests.GetInterests().then(response => setInteresses(response.data))
 	}, [])
 
-	const carrossel = useRef();
-
-	const next = () => {
-		if((cont+1) < data[select]?.photos?.length){
-			setCont(cont + 1)
-		}
-	}
-
-	const prev = () => {
-		if(cont > 0){
-			setCont(cont - 1)
-		}
-	}
-  	
-
-  	const _renderItem = ({item, index}) => {
-      return (
-      	<ImageBackground
-        		style={{ borderRadius: 5, height: '100%' }}
-        		source={{uri: item.photos[cont]?.url || item.photos[cont]?.uri}}
-        		resizeMode='cover'
-        	> 
-    
-      		<View style={{flex: 1, flexDirection: 'row'}}>
-      			<TouchableOpacity style={{flex: 2}} onPress={() => prev()} />
-      			<TouchableOpacity style={{flex: 1}}/>
-      			<TouchableOpacity style={{flex: 2}} onPress={() => next()} />
-      		</View>
-      		<View style={{flexDirection: 'row'}}>
-      			<View style={{backgroundColor: 'rgba(255, 87, 87, 0.25)', height: 55, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-	      			<Icon name="close-outline" size={30} color={'white'} />
-	      		</View>
-	      		<View style={{backgroundColor: 'rgba(24, 178, 76, 0.25)', height: 55, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-	      			<Icon name="checkmark-outline" size={30} color={'white'} />
-	      		</View>
-      		</View>
-        	</ImageBackground>
-      )
-   }
+	const swiper = useRef();
 
    const renderInter = () => {
    	const teres = []
@@ -102,29 +74,63 @@ const index = props => {
 	   })
 	}
 
+	const swipe = (user, index, direcao) => {
+		setSelect(index + 1);
+		helpersHangouts.LikeOrDeslike(user.id, direcao).then(res => console.log(res.data))
+		setCont(0);
+	} 
+
+	const swipeLeft = (user, index, direcao) => {
+		setSelect(index + 1);
+		helpersHangouts.LikeOrDeslike(user.id, direcao).then(res => console.log(res.data))
+		setCont(0);
+		swiper.current.swipeLeft()
+	}
+
+	const swipeRight = (user, index, direcao) => {
+		setSelect(index + 1);
+		shelpersHangouts.LikeOrDeslike(user.id, direcao).then(res => console.log(res.data))
+		setCont(0);
+		swiper.current.swipeRight()
+	}
+
   	return (
     	<SafeAreaView style={styles.container}>
-	    	<View style={{flex: 2}}>
-		    	<Carousel
-	            layout={'default'}
-	            layoutCardOffset={14}
-	            ref={carrossel}
-	            data={data}
-	            sliderWidth={windowWidth}
-	            itemWidth={windowWidth}
-	            renderItem={_renderItem}
-	            useScrollView={true}
-	            inactiveSlideShift={0}
-        			useScrollView={true}
-	            onSnapToItem={index => {setSelect(index), setCont(0)}} 
-	         />
+	    	<View style={{height: height*2}}>
+	    	{
+	    		data.length > 0 ? <CardStack
+		         loop={false}
+		         verticalSwipe={false}
+		         renderNoMoreCards={() => <NoMoreCards voltar={() => props.toggle_tab()}/>}
+		         ref={swiper}
+		      >
+		         {data.map((item, index) => (
+		           	<Card 
+		           		key={index} 
+		           		onSwipedLeft={() => swipe(item, index, 'left')} 
+		           		onSwipedRight={() => swipe(item, index, 'right')}
+		           	>
+		              	<CardItem
+		                	image={item.photos[1]?.url ? item.photos[cont]?.url : null}
+		                	imagens={item.photos}
+		                	onPressLeft={() => swipeLeft(item, index, 'left')}
+                			onPressRight={() => swipeRight(item, index, 'right')}
+                			previa={() => prev()}
+                			proxima={() => next()}
+                			voltar={() => props.toggle_tab()}
+		              	/>
+	            	</Card>
+		         ))}
+		        
+        		</CardStack> : null
+	    	}	
 	    	</View>
-	      <View style={{flex: 1, backgroundColor: '#424343', borderTopLeftRadius: 10, borderTopRightRadius: 10, marginTop: -10}}>
+	      <View style={{height: height, backgroundColor: '#424343', borderTopLeftRadius: 10, borderTopRightRadius: 10}}>
 	      	<View style={{backgroundColor: '#2D2D2D40', margin: 20, flex: 1, borderRadius:20, padding: 10}}>
 	      		<Text style={{fontFamily: Titulo, fontSize: 22, color: 'white', fontWeight: 'bold'}}>
 	      		  	{data[select]?.name} 
 	      		</Text>
-	      		<Text style={{fontSize: 14, fontWeight: '500', marginTop: 10, color: 'white'}}>
+	      		<Text style={{fontSize: 14, fontWeight: '500', marginTop: 10, color: 'white'}} numberOfLines={3}>
 	      		  	{data[select]?.biography} 
 	      		</Text>
 	      		<View style={{paddingTop: 20}}>
@@ -137,34 +143,14 @@ const index = props => {
 	      		</View>
 	      	</View>
 	     	</View>
-	     	<View style={{justifyContent: 'space-between', flexDirection: 'row', padding: 30, position: 'absolute', width: '100%', height: '100%'}}>
-      			<BackButtonSolid callback={(value) => props.toggle_tab(value)}/>
-      			{
-			      	Platform.select({
-			      		ios: <BlurView
-									style={{margin: 20, width: 75, height:44, justifyContent:'center', alignItems: 'center', borderRadius: 20}}
-									blurType="light"
-									blurAmount={0.1}
-									reducedTransparencyFallbackColor="white"
-							>
-								<View style={{flexDirection: 'row'}}>
-									<Icon name="image-outline" size={20} color={'white'} />
-									<Text  style={{fontFamily: Titulo, color: 'white', fontSize: 16, fontWeight: '700', marginHorizontal: 5}}>
-							  			{/*cont+1}/{item.photos?.length*/} 
-									</Text>
-								</View>
-							</BlurView>,
-			      		android: <View	style={[styles.styleViewBlur, {height: 44, width: 75, margin: 10 }]}>
-			      			<View style={{flexDirection: 'row', justifyContent: 'space-evenly', flex:1, alignItems: 'center'}}>
-									<Icon name="image-outline" size={20} color={'white'} />
-									<Text  style={{fontFamily: Titulo, color: 'white', fontSize: 14, fontWeight: '700'}}>
-							  			{cont+1}/{data[select]?.photos?.length} 
-									</Text>
-								</View>
-			      		</View>
-			      	})
-			      }
-      		</View>
+	     	<Modal
+		      animationType="slide"
+		      transparent={true}
+		      visible={modalVisible}
+		      onRequestClose={() => setModalVisible(!modalVisible)}
+		   >
+		   	<Tour closeModal={() => setModalVisible(!modalVisible)}/>
+		   </Modal>
     	</SafeAreaView>
   	)
 };
@@ -186,15 +172,22 @@ const styles = StyleSheet.create({
 		fontWeight: '600',
 		color: Primary
 	},
-	styleViewBlur: {
-		borderRadius: 10,
-		shadowOpacity: 1,
-		shadowColor: '#000',
-		shadowOffset: { width: 10, height: 10 },
-		shadowRadius: 5,
-		elevation: 5,
-		backgroundColor: "rgba(255, 255, 255, 0.3)"
-	}
+	centeredView: {
+    	flex: 1
+  	},
+  	modalView: {
+  		flex: 1,
+    	backgroundColor: 'rgba(0,0,0, 0.6)',
+    	shadowColor: "#000",
+    	shadowOffset: {
+      	width: 0,
+      	height: 2
+    	},
+    	shadowOpacity: 0.25,
+    	shadowRadius: 4,
+    	elevation: 5,
+    	justifyContent: "center"
+  	},
 });
 
 const mapStateToProps = (state) => {
