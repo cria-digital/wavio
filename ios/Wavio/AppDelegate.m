bc47d3bf2118b238849e5784b11c3479c594b434
hervesson <hervessonporto@hotmail.com>
2022-02-16 23:47:51 -0300

    Instalando firebase no ios

--- ios/Wavio/AppDelegate.m
+++ ios/Wavio/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -29,6 +30,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
